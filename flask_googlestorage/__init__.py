6ad0767c1abfef2d45aac5e3ff9e3d31c9f52eca
Santiago Videla <santiago.videla@gmail.com>
2020-06-09 23:46:58 -0300

    ignore flake in package __init__

--- flask_googlestorage/__init__.py
+++ flask_googlestorage/__init__.py
@@ -1,5 +1,7 @@
-from .google_storage import GoogleStorage  # noqa
-from .buckets import Bucket, LocalBucket, CloudBucket  # noqa
-from .exceptions import NotAllowedUploadError, NotFoundBucketError, NotFoundDestinationError  # noqa
+# flake8: noqa
+
+from .google_storage import GoogleStorage
+from .buckets import Bucket, LocalBucket, CloudBucket
+from .exceptions import NotAllowedUploadError, NotFoundBucketError, NotFoundDestinationError
 
 __version__ = "0.1.2"
